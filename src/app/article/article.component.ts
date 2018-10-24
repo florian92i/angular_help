22fd7f46849b879d82a64d83d0f9a8a710df4d71
florian.compiegne <florian.compiegne@eemi.com>
2018-10-24 17:41:13 +0200

    ok

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -10,13 +10,33 @@ export class ArticleComponent implements OnInit {
 
   constructor(private router: Router, private route: ActivatedRoute) {}
 
+  affichage: any;
   articleId: number = null;
-
+  myArticles: any = [
+    {
+      id: 1,
+      name: 'article test home ',
+      date: '2018-09-19',
+      new: true
+    },
+    {
+      id: 2,
+      name: 'article test  home 2',
+      date: '2018-09-19',
+      new: true
+    },
+    {
+      id: 3,
+      name: 'article test  home 3',
+      date: '2018-09-19',
+      new: false
+    }
+  ];
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       if (params['id']) {
         this.articleId = parseInt(params['id'], 10);
-        console.log(this.articleId);
+        this.affichage = this.myArticles.find(hero => hero.id === this.articleId);
       }
     });
    console.log(this.router);
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
-
+  myArticles: any = [
+    {
+      id: 1,
+      name: 'article test home ',
+      date: '2018-09-19',
+      new: true
+    },
+    {
+      id: 2,
+      name: 'article test  home 2',
+      date: '2018-09-19',
+      new: true
+    },
+    {
+      id: 3,
+      name: 'article test  home 3',
+      date: '2018-09-19',
+      new: false
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
